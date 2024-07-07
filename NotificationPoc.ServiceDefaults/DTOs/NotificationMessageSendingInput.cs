f3c4adeae824e52387ca3cd054281ff6d8afffa3
Yongguang Zhu <yoz@microsoft.com>
2024-07-07 20:02:35 +0800

    implement binding-notification-input

---
+++ NotificationPoc.ServiceDefaults/DTOs/NotificationMessageSendingInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationPoc.ServiceDefaults.DTOs
+{
+    public record NotificationMessageSendingInput
+    {
+        public required string Type { get; init; }
+        public required string Title { get; init; }
+        public required string Body { get; init; }
+        public required string[] Recipients { get; init; }
+        public required string AppId { get; init; }
+    }
+}
---
+++ NotificationService/Controllers/ServiceController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NotificationPoc.ServiceDefaults.DTOs;
+using System.Reflection;
+using System.Text;
+
+namespace NotificationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        [HttpPost("/binding-notification-input")]
+        public ActionResult<Guid> Send([FromBody] NotificationMessageSendingInput input)
+        {
+            Console.WriteLine("Received Message: \n" + PrintObject(input));
+            return Guid.NewGuid();
+        }
+
+        private string PrintObject(NotificationMessageSendingInput input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // Get the type of the input object
+            Type type = input.GetType();
+
+            // Get all the properties of the input object
+            PropertyInfo[] properties = type.GetProperties();
+
+            // Iterate through each property and append its name and value to the StringBuilder
+            foreach (PropertyInfo property in properties)
+            {
+                string propertyName = property.Name;
+                object propertyValue = property.GetValue(input);
+
+                sb.AppendLine($"{propertyName}: {propertyValue}");
+            }
+
+            // Return the string representation of the object's properties
+            return sb.ToString();
+        }
+    }
+}
