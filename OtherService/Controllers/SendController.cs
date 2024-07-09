888a5ab21f30b5cf215c29c19d5d7c7e0d0fce40
Yongguang Zhu <yoz@microsoft.com>
2024-07-09 16:21:53 +0800

    add api for webfrontend

---
+++ OtherService/Controllers/SendController.cs
@@ -0,0 +1,26 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NotificationPoc.ServiceDefaults.DTOs;
+
+namespace OtherService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendController : ControllerBase
+    {
+        private readonly DaprClient daprClient;
+
+        public SendController(DaprClient daprClient)
+        {
+            this.daprClient = daprClient;
+        }
+
+        [HttpPost]
+        public IActionResult Send([FromBody] NotificationMessageSendingInput input)
+        {
+            daprClient.InvokeBindingAsync("binding-notification-output", "create", input);
+            return Ok();
+        }
+    }
+}
--- OtherService/Program.cs
+++ OtherService/Program.cs
@@ -4,7 +4,8 @@ builder.AddServiceDefaults();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddDapr();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
